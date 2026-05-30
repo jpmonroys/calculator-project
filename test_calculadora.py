15c6b5a065bdca46708f70c88276f3bfce2247de
Juan Pablo Monroy Soto <jpmonroy@G900603NT440.cencosud.corp>
2026-05-30 11:49:42 -0500

    Intentional failure

--- test_calculadora.py
+++ test_calculadora.py
@@ -1,13 +1,17 @@
 from calculadora import add, subtract, multiply, divide
 
+
 def test_add():
     assert add(2, 3) == 5
+
 
 def test_subtract():
     assert subtract(5, 2) == 3
 
+
 def test_multiply():
     assert multiply(3, 4) == 12
 
+
 def test_divide():
-    assert divide(9, 3) == 2
+    assert divide(9, 3) == 3
